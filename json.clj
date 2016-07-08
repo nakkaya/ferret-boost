e7c5eaee92b15f0bb2f2c41ee311239bfdc6fcdd
Nurullah Akkaya <nurullah@nakkaya.com>
2016-07-08 21:30:32 +0200

    implement json write

---
+++ json.clj
@@ -0,0 +1,17 @@
+(defnative write [col]
+  (on "defined FERRET_STD_LIB"
+      ("boost/property_tree/ptree.hpp"
+       "boost/property_tree/json_parser.hpp")
+      "using boost::property_tree::ptree;
+       using boost::property_tree::read_json;
+       using boost::property_tree::write_json;
+       ptree pt;
+       size_t size = runtime::count(col);
+       for(size_t i = 0; i < size; i+=2){
+          var key = runtime::nth(col, i);
+          var val = runtime::nth(col, i+1);
+          pt.put (key.cast<String>()->to_string(), val.cast<String>()->to_string());
+       }
+       std::ostringstream buf;
+       write_json (buf, pt, false);
+       __result = obj<String>(buf.str().c_str());"))
