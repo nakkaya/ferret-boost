fdd3de60ac96693ba01c45c41e2af72ddd1d804b
Nurullah Akkaya <nurullah@nakkaya.com>
2016-07-11 03:14:00 +0300

    auto garbage collection

--- serial.clj
+++ serial.clj
@@ -1,11 +1,19 @@
+(defobject SerialState
+  (data "boost::asio::io_service io;"
+        "boost::asio::serial_port port;")
+  (equals "return obj<Boolean>(this == o.cast<SerialState>());")
+  (stream_console "fprintf(FERRET_OUTPUT_STREAM, \"SerialState\"); return nil();")
+  (fns
+   ("explicit SerialState() : port(io) { }")
+   ("boost::asio::serial_port& serial_port() { return port;}")
+   ("boost::asio::io_service& io_service() { return io;}"))
+  (force-type))
+
 (defnative open [p b]
   (on "defined FERRET_STD_LIB"
       ("boost/asio/serial_port.hpp"
        "boost/asio.hpp")
-      "var io_ptr = obj<Pointer>(new boost::asio::io_service());
-       boost::asio::io_service & io = io_ptr.cast<Pointer>()->reference<boost::asio::io_service>();
-       var port_ptr = obj<Pointer>(new boost::asio::serial_port(io));
-       boost::asio::serial_port * port = port_ptr.cast<Pointer>()->pointer<boost::asio::serial_port>();
+      "__result  = obj<SerialState>();
 
        // Base serial settings
        boost::asio::serial_port_base::baud_rate BAUD(b.cast<Number>()->as<int>());
@@ -13,35 +21,37 @@
        boost::asio::serial_port_base::parity PARITY( boost::asio::serial_port_base::parity::none );
        boost::asio::serial_port_base::stop_bits STOP( boost::asio::serial_port_base::stop_bits::one );
 
-       try { port->open(p.cast<String>()->to_string()); } catch(const std::exception& e) { return nil(); }
+       try { 
+         __result.cast<SerialState>()->serial_port().open(p.cast<String>()->to_string()); 
+       } catch(const std::exception& e) { 
+         return nil(); 
+       }
 
        // Setup port - base settings
-       port->set_option( BAUD );
-       port->set_option( FLOW );
-       port->set_option( PARITY );
-       port->set_option( STOP );
+       __result.cast<SerialState>()->serial_port().set_option( BAUD );
+       __result.cast<SerialState>()->serial_port().set_option( FLOW );
+       __result.cast<SerialState>()->serial_port().set_option( PARITY );
+       __result.cast<SerialState>()->serial_port().set_option( STOP );"))
 
-       __result = runtime::list(port_ptr,io_ptr);"))
-
-(defnative write [[port-ptr] byte]
+(defnative write [conn byte]
   (on "defined FERRET_STD_LIB"
-      "boost::asio::serial_port & port = port_ptr.cast<Pointer>()->reference<boost::asio::serial_port>();
+      "boost::asio::serial_port& port = conn.cast<SerialState>()->serial_port();
        unsigned char ch = byte.cast<Number>()->as<int>();
        try { boost::asio::write(port, boost::asio::buffer(&ch, 1)); } 
        catch(const std::exception& e) { return nil(); }
        __result = byte;"))
 
-(defnative read [[port-ptr]]
+(defnative read [conn]
   (on "defined FERRET_STD_LIB"
-      "boost::asio::serial_port & port = port_ptr.cast<Pointer>()->reference<boost::asio::serial_port>();
+      "boost::asio::serial_port& port = conn.cast<SerialState>()->serial_port();
        unsigned char ch;
        try { boost::asio::read(port, boost::asio::buffer(&ch, 1)); } catch(const std::exception& e) { return nil(); }
        __result = obj<Number>((ferret::number_t)ch);"))
 
-(defnative read-line [[port-ptr]]
+(defnative read-line [conn]
   (on "defined FERRET_STD_LIB"
       ("boost/algorithm/string.hpp")
-      "boost::asio::serial_port & port = port_ptr.cast<Pointer>()->reference<boost::asio::serial_port>();
+      "boost::asio::serial_port& port = conn.cast<SerialState>()->serial_port();
        boost::asio::streambuf b;
        std::ostringstream line;
        try { boost::asio::read_until(port, b, \"\\r\\n\"); } catch(const std::exception& e) { return nil(); }
@@ -52,11 +62,9 @@
 
 (defnative close [[port io]]
   (on "defined FERRET_STD_LIB"
-      "boost::asio::serial_port & p = port.cast<Pointer>()->reference<boost::asio::serial_port>();
-       p.cancel();
-       p.close();
-       boost::asio::io_service & i = io.cast<Pointer>()->reference<boost::asio::io_service>();
-       i.stop();
-       i.reset();
-       delete port.cast<Pointer>()->pointer<boost::asio::serial_port>();;
-       delete io.cast<Pointer>()->pointer<boost::asio::io_service>();"))
+      "boost::asio::serial_port& port = conn.cast<SerialState>()->serial_port();
+       port.cancel();
+       port.close();
+       boost::asio::io_service& io = conn.cast<SerialState>()->io_service();
+       io.stop();
+       io.reset();"))
