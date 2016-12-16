8d202ca87930861845b1ad87d6f610f6a0e0dc32
Nurullah Akkaya <nurullah@nakkaya.com>
2016-12-16 18:01:55 +0000

    Update file-system.clj

--- file-system.clj
+++ file-system.clj
@@ -1,4 +1,4 @@
 (defnative exists [f]
   (on "defined FERRET_STD_LIB"
       ("boost/filesystem.hpp")
-      "__result = obj<Boolean>(boost::filesystem::exists(f.cast<String>()->to_string()));"))
+      "__result = obj<Boolean>(boost::filesystem::exists(f.to<std::string>()));"))
