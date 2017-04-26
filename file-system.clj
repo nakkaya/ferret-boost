c45a53c99432d214645b4b04960d9bc8e791a2f3
Nurullah Akkaya <nurullah@nakkaya.com>
2017-04-26 14:07:42 +0300

    implement create-directory file-seq

--- file-system.clj
+++ file-system.clj
@@ -2,3 +2,18 @@
   (on "defined FERRET_STD_LIB"
       ("boost/filesystem.hpp")
       "__result = obj<boolean>(boost::filesystem::exists(string::to<std::string>(f)));"))
+
+(defnative create-directory [f]
+  (on "defined FERRET_STD_LIB"
+      ("boost/filesystem.hpp")
+      "__result = obj<boolean>(boost::filesystem::create_directory(string::to<std::string>(f)));"))
+
+(defnative file-seq [f]
+  (on "defined FERRET_STD_LIB"
+      ("boost/filesystem.hpp")
+      "namespace fs = boost::filesystem;
+       fs::path p (string::to<std::string>(f));
+       fs::recursive_directory_iterator end_itr;
+
+       for(fs::recursive_directory_iterator itr(p); itr != end_itr; ++itr)
+          __result =  runtime::cons(obj<string>(itr->path().string()),__result);"))
